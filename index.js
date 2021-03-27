d55e2de2c44b9514897efb7451ba35c2081dfd00
dposadacalle <posadacalled@gmail.com>
2021-03-26 20:24:57 -0500

    Modularize Express and Add config file

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-
 const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
-});
+const app = require('./server');
+const config = require('./server/config');
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+const { port } = config.server;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`Server running at port: ${port}`);
 });
