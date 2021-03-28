e82c3f6d01b1bd008e17a198cea27acba53406aa
dposadacalle <posadacalled@gmail.com>
2021-03-27 22:45:28 -0500

    Setting API mount point and basic router for Tasks

--- server/index.js
+++ server/index.js
@@ -1,41 +1,23 @@
 //  server/index.js
 const express = require('express');
-
 const requestId = require('express-request-id')();
 
 const logger = require('./config/logger');
+const api = require('./api');
 
+// Init App
 const app = express();
 
 // Setup middleware
 app.use(requestId);
 app.use(logger.requests);
 
-// No route found handler
-app.use((req, res, next) => {
-  const message = 'Route not found';
-  const statusCode = 404;
-
-  logger.warn(message);
-
-  res.status(statusCode);
-
-  res.json({
-    message,
-  });
-});
+// Setup router and routes
+app.use('/api', api);
 
 app.get('/', (req, res, next) => {
   res.json({
     message: 'Welcome to the API',
-  });
-});
-
-// No route found handler
-app.use((req, res, next) => {
-  res.status(404);
-  res.json({
-    message: 'Error. Route not found',
   });
 });
 
