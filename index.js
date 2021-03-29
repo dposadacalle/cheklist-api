538cdfc5f4f34a8285eef58f2de355b56335fb24
dposadacalle <posadacalled@gmail.com>
2021-03-28 23:39:28 -0500

    Add mongoose and connect to database

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const http = require('http');
 
 const app = require('./server');
 const config = require('./server/config');
+const database = require('./server/database');
+
+// Connect to database
+database.connect(config.database, {});
 
 const { port } = config.server;
 
 const server = http.createServer(app);
 
 server.listen(port, () => {
-    console.log(`Server running at port: ${port}`);
+  console.log(`Server running at port: ${port}`);
 });
---
+++ server/database.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const logger = require('./config/logger');
+
+exports.connect = (
+  { protocol = 'mongodb', url, username = '', password = '' },
+  options = {}
+) => {
+  let dburl = '';
+
+  // Required auth
+  if (username && password) {
+    dburl = `${protocol}://${username}:${password}@${url}`;
+  } else {
+    // eslint-disable-next-line no-unused-vars
+    dburl = `${protocol}://${url}`;
+  }
+
+  mongoose.connect(dburl, {
+    ...options,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+
+  mongoose.connection.on('open', () => {
+    logger.info('Database connected');
+  });
+
+  mongoose.connection.on('close', () => {
+    logger.info('Database disconnected');
+  });
+
+  mongoose.connection.on('error: ', (err) => {
+    logger.error(`Database connection error: ${err}`);
+  });
+
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+      logger.info('Database connection disconnected through app termination');
+      process.exit(0);
+    });
+  });
+};
+
+exports.disconnect = () => {
+  mongoose.connection.close(() => {
+    logger.info('Database disconnected');
+  });
+};
